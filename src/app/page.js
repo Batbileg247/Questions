1fed50716003555e6e7143af2bd3d69b1a57a5c4
25lp3625 User <25lp3625@administrators-iMac.local>
2026-01-09 12:56:08 +0800

    hellow

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,87 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+
+const qArr = [
+  {
+    id: 1,
+    title: "Do I have to allow the use of cookies?",
+    answer:
+      "Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.",
+  },
+  {
+    id: 2,
+    title: "How do I change my My Page password?",
+    answer:
+      "Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.",
+  },
+  {
+    id: 3,
+    title: "What is BankID?",
+    answer:
+      "Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.",
+  },
+  {
+    id: 4,
+    title: "Whose birth number can I use?",
+    answer:
+      "Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.",
+  },
+  {
+    id: 5,
+    title: "When do I recieve a password ordered by letter?",
+    answer:
+      "Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan",
+  },
+];
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="h-screen w-screen bg-[#cffafe] flex items-center justify-center">
+      <div className="p-12 bg-white flex flex-col items-center">
+        <h1 className="w-180 text-5xl flex justify-center mb-12">Questions</h1>
+        <Quest />
+      </div>
     </div>
   );
 }
+
+const Quest = () => {
+  const [openId, setOpenId] = useState([]);
+  const [list, setList] = useState(qArr);
+  const [isVisible, setIsVisible] = useState(false);
+  console.log(openId);
+
+  const showText = (id) => {
+    setIsVisible(!isVisible);
+    setOpenId((prev) => [...prev, id]);
+  };
+
+  return (
+    <div>
+      {list.map((item) => {
+        const isOpen = openId.includes(item.id) && isVisible;
+
+        return (
+          <div
+            key={item.id}
+            className="mb-8 p-4 flex flex-col items-center rounded-sm border-2 border-black/25 shadow-2xl"
+          >
+            <div className="w-167 flex items-center justify-between">
+              <h2 className="text-xl font-semibold">{item.title}</h2>
+
+              <button
+                onClick={() => showText(item.id)}
+                className="h-8 w-8 cursor-pointer bg-[#164e63] rounded-full text-white"
+              >
+                {isOpen ? "-" : "+"}
+              </button>
+            </div>
+
+            {isOpen && <div className="w-167 text-gr mt-2">{item.answer}</div>}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
